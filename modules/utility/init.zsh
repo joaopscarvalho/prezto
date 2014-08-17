969f76e2a958846dc899f02ca8f5fdcf42cb4bc5
jcarvalho <jcarvalho@isr.ist.utl.pt>
2014-08-17 14:25:25 +0100

    Change config.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -90,10 +90,11 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
+#alias l='ls -1A'         # Lists in one column, hidden files.
+alias l='ls -CF'         # Lists by columns and append indicator
 alias ll='ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
+alias la='ll -AF'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
